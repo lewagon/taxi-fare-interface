69e2697cf7496709b77360b3020c98ec38406df8
Adenegan Oluwafeyisayomi <70474089+Feyisayomi@users.noreply.github.com>
2021-02-26 11:58:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const algoliaPlacesApiAppId = 'plU4N8HG6QWK';
 const algoliaPlacesApiKey = '1131438afb49f60a48ed468c5af189b8';
 const mapboxApiToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
-const taxiFareApiUrl = 'http://localhost:8000/predict_fare/';
+const taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict_fare/';
 
 const displayMap = (start, stop) => {
   mapboxgl.accessToken = mapboxApiToken;
