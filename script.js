823f3806319f46fb7110a302e4cc9fa2e1b05370
kevin robert <kevin.j.robert@gmail.com>
2021-05-17 16:20:30 +0200

    handle api response

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ const predict = () => {
       .then(data => {
         document.getElementById('fare').classList.remove('d-none');
         const fareResult = document.getElementById('predicted-fare');
-        const fare = Math.round(data['prediction'] * 100) / 100
+        const fare = Math.round(data['fare'] * 100) / 100
         fareResult.innerText = `$${fare}`;
       })
       .catch((error) => {
