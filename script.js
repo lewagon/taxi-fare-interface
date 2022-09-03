2127a918c8e6157f360d18b332d74e93773d5be5
gilles <103757106+gilles4@users.noreply.github.com>
2022-09-03 11:30:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoiZ2lsbGVzYmFiYSIsImEiOiJjbDdsbzg2bGEwaWN2M29wOXBiNHZlM3NxIn0.8-17WuPLIC5KQPY9p2LSHQ';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
